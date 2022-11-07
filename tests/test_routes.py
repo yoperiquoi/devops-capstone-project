3648a74012d2be49251b77ba72b8f4f240985e16
yoperiquoi <yoann_63115@hotmail.fr>
2022-11-07 17:54:22 -0500

    add delete feature and tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -148,3 +148,10 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
